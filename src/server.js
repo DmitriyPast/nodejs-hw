9f15afc0f71c5550806d1d22c678b1a5afcd7899
Your Name <your_email>
2025-09-28 20:23:14 +0300

    fix cors not being imported

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 // src/server.js
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3030;
 
 // Middleware для парсингу JSON
 app.use(express.json());
