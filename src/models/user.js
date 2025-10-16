109e30a987d5496394102f2f45f997d7a33f34e0
Your Name <your_email>
2025-10-16 23:03:22 +0300

    user.js metods.tojson

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,6 @@
 import { model, Schema } from 'mongoose';
 
-const userSchema = new Schema(
+export const userSchema = new Schema(
   {
     username: { type: String, trim: true },
     email: { type: String, unique: true, required: true, trim: true },
@@ -8,5 +8,17 @@ const userSchema = new Schema(
   },
   { timestamps: true, versionKey: false },
 );
+
+// Перевизначаємо метод toJSON
+userSchema.methods.toJSON = f1;
+function f() {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+}
+
+function f1() {
+  return delete this.toObject().password;
+}
 
 export const User = model('User', userSchema);
