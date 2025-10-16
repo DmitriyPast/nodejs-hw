2b68dceb215ab682727ad5f963f76ffc8f601462
Your Name <your_email>
2025-10-16 21:14:35 +0300

    add user model

---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: { type: String, trim: true },
+    email: { type: String, unique: true, required: true, trim: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const User = model('user', userSchema);
