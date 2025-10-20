40afec466e8db3b9102d0415284934b5a53350df
Your Name <your_email>
2025-10-20 10:26:01 +0300

    Метод toJSON видаляє поле password, використовуючи логічний вираз (delete obj.password && obj), що може повернути false, якщо delete obj.password поверне false. Це може призвести до того, що метод поверне false замість об'єкта. Метод завжди має повертати об'єкт без поля password.

--- src/models/user.js
+++ src/models/user.js
@@ -12,13 +12,14 @@ export const userSchema = new Schema(
 // Перевизначаємо метод toJSON
 userSchema.methods.toJSON = function () {
   const obj = this.toObject();
-  return delete obj.password && obj;
+  delete obj.password;
+  return obj;
 };
-// function f() {
+
+//   function () {
 //   const obj = this.toObject();
-//   delete obj.password;
-//   return obj;
-// }
+//   return delete obj.password && obj;
+// }; Why not to use this ^: Метод toJSON видаляє поле password, використовуючи логічний вираз (delete obj.password && obj), що може повернути false, якщо delete obj.password поверне false. Це може призвести до того, що метод поверне false замість об'єкта. Метод завжди має повертати об'єкт без поля password.
 
 userSchema.pre('save', function (next) {
   if (!this.username) this.username = this.email;
