64b2c82943654b9299ad693eb1a038ecffaf7354
your_name <pastushenkodmitry2003@gmail.com>
2025-10-24 19:32:23 +0300

    env updates & add SMTP server connection service

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,16 @@
+// src/utils/sendEmail.js
+
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+export async function sendEmail(options) {
+  return await transporter.sendMail(options);
+}
