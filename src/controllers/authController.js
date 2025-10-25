a387f2b8a9d5971a81032785b5750f32b3ca52ee
your_name <pastushenkodmitry2003@gmail.com>
2025-10-25 22:26:29 +0300

    fix-01

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,7 +7,7 @@ import handlebars from 'handlebars';
 import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
-import { sendEmail } from '../utils/sendEmail.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 export async function registerUser(req, res, next) {
   const { email, password } = req.body;
