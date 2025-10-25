692a41e3aed31c6c7869a031354a848330cc9896
your_name <pastushenkodmitry2003@gmail.com>
2025-10-25 22:10:17 +0300

    update avatar implemented stream to cloudinary and update user in DB

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,13 @@
 import createHttpError from 'http-errors';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { User } from '../models/user.js';
 
 export async function updateUserAvatar(req, res, next) {
   if (!req.file) return next(createHttpError(400, 'No file'));
 
-  res.status(200).json({ url: 'gay' });
+  const { secure_url } = await saveFileToCloudinary(req.file.buffer);
+
+  await User.updateOne({ _id: req.user._id }, { avatar: secure_url });
+
+  res.status(200).json({ url: secure_url });
 }
